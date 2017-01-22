781dcdd5efc849f3039bf86aad12f3f83f4b11b6
ManuelSaleta <ManuelSaleta@users.noreply.github.com>
2017-01-22 12:20:46 -0500

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    
+   
+    List<string> lvl = new List<String>();
+
+    
+	// Use this for initialization
+	void Start () {
+
+        Addlevel("level_0");
+        Addlevel("Level1_TheOffice");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        quit();
+        pauseGame();
+	}
+
+    // quit the game
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void pauseGame()
+    {
+
+       //nput.GetButton("PAUSE");
+        
+    }
+
+    //Adds level object to the list of levels.
+    private void Addlevel(String lvlName)
+    {
+        lvl.Add(lvlName);
+    }
+
+   public void LevelPicker(int index)
+    {
+        if (index > lvl.Count)
+            print("Cant do");
+        else
+       SceneManager.LoadScene(lvl[index]);
+    }
+
+}
+
